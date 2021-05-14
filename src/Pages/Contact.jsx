7a8031cf92b23bec65c2a04d9441a67bea80cb4d
brandon <bberning.web@gmail.com>
2021-05-14 09:41:31 -0400

    added the contact page. Not final stylings but its very close

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,7 +5,18 @@ const Contact = () =>  {
 
     return (
         <div className='contact'>
-            contact
+            <div className='contact-info-wrapper'>
+                <h1>Contact</h1>
+
+                <div className='contact-info'>
+                    <h2>Email</h2>
+                    <h4>example@example.com</h4>
+
+                    <h2>Address</h2>
+                    <h4>123 Demo Street</h4>
+                    <h4>New York, NY 12345</h4>
+                </div>
+            </div>
         </div>
         )
 }
