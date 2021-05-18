31abecb4ef2fde5bdcc5c0e29d0429f0fea24b28
brandon <bberning.web@gmail.com>
2021-05-18 10:17:36 -0400

    added functionality to slide buttons

--- src/Comps/Slide.jsx
+++ src/Comps/Slide.jsx
@@ -3,15 +3,29 @@ import ProjectDisplaySlideBox from './ProjectDisplaySlideBox'
 import '../Styles/Slide.scss'
 
 const Slide = ({data}) =>  {
-    const [episodes, setEpisodes] = useState([data[2], data[1], data[0]]);
+    const [episodes, setEpisodes] = useState(data.slice(3, 6).reverse());
+    const [Shift, setShift] = useState(false);
 
- 
 
+    const ShiftSlide = (direction) => {
+        if (direction == 'left') {
+            setEpisodes(data.slice(3, 6).reverse())
+        } else {
+            setEpisodes(data.slice(0, 3).reverse())
+        }
+        setShift(!Shift)
+    }
+
+
+    console.log(episodes)
     return (
         <div className='slide'>
-            {episodes.map((episode, i) => {
-                return <ProjectDisplaySlideBox project={episode} key={i}/>
-            })}
+            <div className='slide-controls'><i className={Shift ? "not-active fas fa-chevron-left" : 'active fas fa-chevron-left'} onClick={() => ShiftSlide('left')}></i><i class={Shift ? "active fas fa-chevron-right" : 'not-active fas fa-chevron-right'} onClick={() => ShiftSlide('right')}></i></div>
+            <div className='slide-projects'>
+                {episodes.map((episode, i) => {
+                    return <ProjectDisplaySlideBox project={episode} key={i}/>
+                })}
+            </div>
         </div>
         )
 }
